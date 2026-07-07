e3aa732fa0b3799b6ea2e690a66f3ba850876aa6
Eric <ericj6215@gmail.com>
2026-07-07 20:28:37 +0900

    Practice1

---
+++ day02/practice1.js
@@ -0,0 +1,35 @@
+alert("어서오세요")
+
+console.log("[주문접수]")
+
+let name1 = prompt("이름을 작성해주세요")        
+console.log("이름:")
+console.log(name1)
+
+let beverage = prompt("원하는 음료를 작성해주세요")        
+console.log("음료:") 
+console.log(beverage)
+
+let count = prompt("수량을 작성해주세요")        
+console.log("수량:")
+console.log(count)
+
+
+console.log("[운동기록]")
+
+let name2 = prompt("이름을 작성해주세요")        
+console.log("이름:")
+console.log(name2)
+
+let type = prompt("운동 종류")        
+console.log("운동종류:") 
+console.log(type)
+
+let time = prompt("운동시간")        
+console.log("수량:")
+console.log(time)
+
+let cal = 7 * time  
+console.log("총칼로리:")
+console.log(cal)
+

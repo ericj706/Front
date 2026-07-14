a4a29dba466215b0d8c9e494df596ed5062e5f17
Eric <ericj6215@gmail.com>
2026-07-14 11:58:27 +0900

    day07_12:00

--- day07/exam2.js
+++ day07/exam2.js
@@ -1,16 +1,21 @@
 const major = [
-{majorCode : '0000', univName:'성결대', 
-    majorName: '산업공학', 담당자 : '방*영' , 
+{majorCode : '100', univName:'성결대', 
+    majorName: '산업경영공학', 담당자 : '방*영' , 
     전화번호 : '031 - 467 - 8059', 위치 : '성결관', 학부명id : '글경'},
 
-{majorCode : '0001', univName:'성결대', 
+{majorCode : '101', univName:'성결대', 
     majorName: '컴퓨터공학', 담당자 : '임*수' , 
     전화번호 : '031 - 467 - 8341', 위치 : '성결관', 학부명id : 'IT'},
 
-{majorCode : '0002', univName:'성결대', 
-    majorName: '정보통신', 담당자 : '정*래' , 
-    전화번호 : '031 - 467 - 8073', 위치 : '성결관', 학부명id : 'IT'}]
+{majorCode : '102', univName:'성결대', 
+    majorName: '정보통신공학', 담당자 : '정*래' , 
+    전화번호 : '031 - 467 - 8073', 위치 : '성결관', 학부명id : 'IT'},
+
+{majorCode : '103', univName:'성결대', 
+    majorName: '미디어소프트웨어', 담당자 : '김*이' , 
+    전화번호 : '031 - 467 - 8348', 위치 : '성결관', 학부명id : 'IT'}
+]
 
 
 //학과정보 : 학과id, 학교명, 학과명, 담당자, 전화번호, 위치, 학부명id
-console.log(major[1].majorName)
+console.log(major[2].majorCode)

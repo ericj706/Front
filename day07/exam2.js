8f95e2969eb93292f7ee5873549ca2ce0c74a74c
Eric <ericj6215@gmail.com>
2026-07-14 11:53:33 +0900

    day07_12:00

---
+++ day07/exam2.js
@@ -0,0 +1,16 @@
+const major = [
+{majorCode : '0000', univName:'성결대', 
+    majorName: '산업공학', 담당자 : '방*영' , 
+    전화번호 : '031 - 467 - 8059', 위치 : '성결관', 학부명id : '글경'},
+
+{majorCode : '0001', univName:'성결대', 
+    majorName: '컴퓨터공학', 담당자 : '임*수' , 
+    전화번호 : '031 - 467 - 8341', 위치 : '성결관', 학부명id : 'IT'},
+
+{majorCode : '0002', univName:'성결대', 
+    majorName: '정보통신', 담당자 : '정*래' , 
+    전화번호 : '031 - 467 - 8073', 위치 : '성결관', 학부명id : 'IT'}]
+
+
+//학과정보 : 학과id, 학교명, 학과명, 담당자, 전화번호, 위치, 학부명id
+console.log(major[1].majorName)
